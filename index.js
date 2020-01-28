94839234946d7cbee657006fde90989be46786ef
octavian-regatun <octavian.regatun@outlook.com>
2020-01-28 17:17:05 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
 
-app.get('/hello', (req, res) => res.send('Hello World!'))
+var http = require('http');
+var server = http.Server(app);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
+});
