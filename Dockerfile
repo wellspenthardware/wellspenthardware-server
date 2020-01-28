13b6a72207dc5f71c4b5508410e9be5a78b1c09e
octavian-regatun <octavian.regatun@outlook.com>
2020-01-28 16:49:34 +0200

    .

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:10-alpine
-WORKDIR /app
-COPY package.json .
-RUN npm install
-COPY index.js .
-CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
